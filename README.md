b38be9206f7472cebd3616dbaa729936b299cdfd
David Inyang-Etoh <dinyangetoh@gmail.com>
2018-03-02 07:38:15 +0100

    added readme file

---
+++ README.md
@@ -0,0 +1,26 @@
+# Invoice Template with Express, Handlebars and Bootstrap
+
+A simple app that implements fixed and responsive versions of an invoice template with ExpressJs, Handlebars and Bootstrap.
+
+## Installation
+
+You have Node installed on you machine 
+
+Clone repo 
+`git clone https://github.com/dinyangetoh/express-invoice-template.git`
+
+Navigate to the project root
+`cd express-invoice-template`
+
+Install npm dependencies
+`npm install`
+
+Launch the project
+`node index` or `node index.js`
+
+View project on `http://localhost:8087`
+
+
+## Contributor
+
+David Inyang-Etoh @dinyangetoh
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var serveStatic = require('serve-static');
 
 // Setup server port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8087;
 
 // app.use(serveStatic('/', { 'index': ['default.html', 'default.htm'] }));
 
@@ -17,20 +17,20 @@ app.use(express.static(__dirname + '/assets'));
 // app.use(express.static('assets'));
 // Web routes
 // app.use('/static', express.static(path.join(__dirname, '/assets')))
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
     res.sendFile('index.html');
 
 });
-app.get('/fixed-invoice', function(req, res) {
+app.get('/fixed-invoice', function (req, res) {
     res.sendFile(path.join(__dirname + '/fixed-invoice.html'));
 });
 
 
-app.get('/fluid-invoice', function(req, res) {
+app.get('/fluid-invoice', function (req, res) {
     res.sendFile(path.join(__dirname + '/fluid-invoice.html'));
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Running on port " + port);
 });
